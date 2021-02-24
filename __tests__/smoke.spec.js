3ec81dea7ab90b721b5ec1667b1153763a311fea
BOUDROUA Imad <Imad.Boudroua@bordeaux-inp.fr>
2021-02-24 20:21:41 +0100

    prepare project for different type of tests including coverage

---
+++ __tests__/smoke.spec.js
@@ -0,0 +1,9 @@
+import 'react-native'
+import React from 'react'
+import HomeScreen from '../src/screens/HomeScreen'
+import renderer from 'react-test-renderer'
+describe('truth', () => {
+  it('is true', () => {
+    expect(true).toEqual(true);
+  });
+});
